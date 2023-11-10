49913fe64629e7f38c2b5fd6b48caa22aa8f91c7
yohanii <dygks924@gmail.com>
2023-11-10 16:59:08 +0900

    refactor: BeforeEach로 testOrder 만들어주기

--- src/test/java/christmas/service/MenuServiceTest.java
+++ src/test/java/christmas/service/MenuServiceTest.java
@@ -14,20 +14,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MenuServiceTest {
 
-    MenuService menuService = new MenuService();
+    private final MenuService menuService = new MenuService();
+    private Order testOrder;
 
-    @Test
-    void getTotalPrice() {
+    @BeforeEach
+    void setUp() {
         Map<Menu, Integer> orderStore = new HashMap<>();
         orderStore.put(Menu.CAESAR_SALAD, 1);
         orderStore.put(Menu.BARBECUE_RIB, 1);
+        orderStore.put(Menu.T_BONE_STEAK, 1);
         orderStore.put(Menu.CHOCO_CAKE, 2);
         orderStore.put(Menu.ZERO_COKE, 3);
-        Order order = new Order(orderStore);
+        testOrder = new Order(orderStore);
+    }
 
-        int result = menuService.getTotalPrice(order);
+    @Test
+    void getTotalPrice() {
+        int result = menuService.getTotalPrice(testOrder);
 
-        int totalPrice = Menu.CAESAR_SALAD.getPrice() + Menu.BARBECUE_RIB.getPrice() + Menu.CHOCO_CAKE.getPrice() * 2 + Menu.ZERO_COKE.getPrice() * 3;
+        int totalPrice = Menu.CAESAR_SALAD.getPrice() + Menu.BARBECUE_RIB.getPrice() + Menu.T_BONE_STEAK.getPrice() + Menu.CHOCO_CAKE.getPrice() * 2 + Menu.ZERO_COKE.getPrice() * 3;
         assertThat(result).isEqualTo(totalPrice);
     }
 }
