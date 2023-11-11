833eb6c5fcec25265d8902ec98ae5e000b9cd81c
yohanii <dygks924@gmail.com>
2023-11-11 17:25:01 +0900

    refactor: 에러메세지 수정

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -42,7 +42,7 @@ public class InputView {
             }
             return date;
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
 
